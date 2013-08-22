de75087292a9379cd8a9f920f7a7e15d4b743705
Ígor Bonadio <igorbonadio@gmail.com>
2013-08-22 15:03:19 -0300

    add and clear tiles

--- www/js/index.js
+++ www/js/index.js
@@ -1,6 +1,8 @@
 var app = {
   // Application Constructor
   initialize: function() {
+    this.lastId = 0;
+    this.colorSchema = ["#DD1E2F", "#EBB035", "#06A2CB", "#218559", "#D0C6B1", "#192823", "#D4E7ED", "#EB8540", "#B06A3B", "#AB988B"]
     this.bindEvents();
   },
 
@@ -9,6 +11,8 @@ var app = {
     $(window).resize(function(){
       app.resizeTiles(130, 10);
     });
+    $("#clear").on("click", this.clear);
+    $("#add").on("click", this.add);
   },
 
   onDeviceReady: function() {
@@ -21,5 +25,17 @@ var app = {
     var tileWidth = deviceWidth / ntiles;
     $(".tile").css("width", tileWidth-(2*borderSize));
     $(".tile").css("height", tileWidth-(2*borderSize));
-  }
+  },
+
+  clear: function(e) {
+    app.lastId = 0;
+    $(".app").html("");
+  },
+
+  add: function(e) {
+    app.lastId = app.lastId + 1;
+    $(".app").prepend('<div class="tile" id="tile-' + app.lastId + '"></div>');
+    var color = app.colorSchema[Math.floor(Math.random()*app.colorSchema.length)];
+    $("#tile-" + app.lastId).css("background-color", color)
+  },
 };
